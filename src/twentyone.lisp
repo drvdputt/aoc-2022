01d7c134cea6aeb4b8fa15260d6ae24424e76b1a
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2022-12-25 13:42:55 +0100

    Write down bisection idea for twentyone part 2

--- src/twentyone.lisp
+++ src/twentyone.lisp
@@ -57,21 +57,45 @@
           do (setf (monkey-value m) nil)))
 
 (defun do-part2 (lines)
-  (let* ((table (parse-monkey-table lines))
+  (let* ((geuss-min 2647078177740) ;; we found two numbers. Guess min has the value too big,
+                                    ;; and guess max has it to small. So do bisection between
+                                    ;; those.
+         (guess-max (* 2 guess-min))
+         (table (parse-monkey-table lines))
          (root-dep1 (monkey-dep1 (gethash "root" table)))
          (root-dep2 (monkey-dep2 (gethash "root" table))))
     ;; (print (loop for k being each hash-key of table collect k))
     ;; (print root-dep1)
     ;; (print root-dep2)
-    (loop for humn-value from 0 to 16
+    (loop for humn-value from (- guess 16) to (+ guess 16)
           do (reset-monkey-values table)
           if (progn
                (setf (monkey-value (gethash "humn" table)) humn-value)
                (let* ((v1 (process-monkeys table root-dep1))
                       (v2 (process-monkeys table root-dep2)))
-                 (print (list v1 v2))
+                 (print (list humn-value v1 v2))
                  (= v1 v2)))
             return humn-value)))
 
-;; Part two needs some human attention. v2 is always equal to 
-  
+;; Part two needs some human attention. v2 is independent of humn, and always equal to
+;; 34588563455325
+
+;; with humn == 0, we get 
+;; 90177205187895
+;; 34588563455325
+
+;; so we need to go down by
+;; 55588641732570
+
+;; going from 4 to 10 (+ 6)
+;; we go from
+;; 90177205187781
+;; to
+;; 90177205187655
+;; which is a difference of 126 over a distance of 6
+
+;; (floor 55588641732570 126) is 441179696290 decrements
+;; times a distance of 6 gives 2647078177740
+;; so we explore around that number
+
+;; let's try bisection search!
