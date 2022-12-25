1f13e30f8a8900fd9b370385d8b66e9b51d2641b
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2022-12-25 13:31:22 +0100

    Brute force + human intuition code for twentyone part 2

--- src/twentyone.lisp
+++ src/twentyone.lisp
@@ -49,3 +49,29 @@
 (defun do-puzzle (lines start)
   (let ((table (parse-monkey-table lines)))
     (process-monkeys table start)))
+
+(defun reset-monkey-values (monkey-table)
+  (loop for m being each hash-value of monkey-table
+        ;; if monkey has an operation, reset its value to nil
+        if (monkey-operation m)
+          do (setf (monkey-value m) nil)))
+
+(defun do-part2 (lines)
+  (let* ((table (parse-monkey-table lines))
+         (root-dep1 (monkey-dep1 (gethash "root" table)))
+         (root-dep2 (monkey-dep2 (gethash "root" table))))
+    ;; (print (loop for k being each hash-key of table collect k))
+    ;; (print root-dep1)
+    ;; (print root-dep2)
+    (loop for humn-value from 0 to 16
+          do (reset-monkey-values table)
+          if (progn
+               (setf (monkey-value (gethash "humn" table)) humn-value)
+               (let* ((v1 (process-monkeys table root-dep1))
+                      (v2 (process-monkeys table root-dep2)))
+                 (print (list v1 v2))
+                 (= v1 v2)))
+            return humn-value)))
+
+;; Part two needs some human attention. v2 is always equal to 
+  
