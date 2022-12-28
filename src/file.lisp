ffe7ba521f8f729961b55e1f1b0a4b821fe82409
Dries Van De Putte <drvdputt.vandeputte@ugent.be>
2022-12-28 16:57:12 +0100

    Make read-array more lispy

--- src/file.lisp
+++ src/file.lisp
@@ -9,19 +9,17 @@
 ;; assumes that all lines are of equal length
 ;; supported read-in types: numbers, chars
 (load "string.lisp")
-(defun read-array (fn type-string)
+(defun read-array (fn type)
   (let* ((lines (read-lines fn))
 	 (nrow (length lines))
 	 (ncol (length (first lines)))
-	 (type (if (string= type-string "chars")
-		   'char
-		   'integer))
 	 (a (make-array (list nrow ncol) :element-type type)))
     (loop for l in lines
 	  for i from 0
-	  with f = (if (string= type-string "chars")
-		       (lambda (line) (loop for c across line collect c))
-		       (function find-all-int))
+	  with f = (cond ((eq type 'char)
+		          (lambda (line) (loop for c across line collect c)))
+                         ((eq type 'integer)
+		          (function find-all-int)))
 	  do (loop for x in (funcall f l)
 		   for j from 0
 		   do (setf (aref a i j) x)))
